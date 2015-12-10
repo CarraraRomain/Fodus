46c5060ad25e9cd857ead9050880a25a32a484e2
AchilleAsh <achille.ash@gmail.com>
2015-12-10 21:30:15 +0100

    update code generation step

---
+++ tools/pydia2code/clean.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import lxml.etree as etree
+import yaml
+import os
+import glob
+import datetime
+
+file = open("config.yml", "r")
+config = yaml.load(file)
+
+# Conf
+tmp_folder = config["directories"]["temp"]
+inp_folder = config["directories"]["input"]
+out_folder = config["directories"]["output"]
+
+print("Cleaning folders...")
+for namespace in config["namespaces"]:
+    for f in glob.glob(out_folder + namespace + "/*.hpp"):
+        os.unlink(f)
--- tools/pydia2code/pydia2code.py
+++ tools/pydia2code/pydia2code.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import lxml.etree as etree
 import yaml
 import os
