88228a92cebe463add9b920109db70f4c8e73c9a
AchilleAsh <achille.ash@gmail.com>
2016-01-07 20:33:28 +0100

    update scripts

---
+++ boot.sh
@@ -0,0 +1,8 @@
+git submodule init
+git submodule update
+cd lib/bonefish
+git submodule init
+git submodule update
+cd ../boost
+./bootstrap.sh --with-toolset=gcc --with-libraries=system,thread,program_options
+./b2
