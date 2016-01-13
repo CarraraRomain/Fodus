7a57b704824ff04fdecbc032bc310af1895327da
AchilleAsh <achille.ash@gmail.com>
2016-01-13 18:11:40 +0100

    fix boot.sh

--- boot.sh
+++ boot.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 git submodule init
 git submodule update
-cd lib/bonefish
+
+cd lib/boost
+./bootstrap.sh --with-toolset=gcc --with-libraries=serialization,regex,system,thread,program_options
+./b2 -j 4
+cd ../bonefish
 git submodule init
 git submodule update
 rm -r build
 mkdir build
 cd build
 cmake .. -DBOOST_ROOT=../boost/
-cd ../../boost
-./bootstrap.sh --with-toolset=gcc --with-libraries=system,thread,program_options
-./b2 -j 4
+
+
